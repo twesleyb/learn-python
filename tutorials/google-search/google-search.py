5e6901a4d53fef6c333aa4175ad65501ff436c4c
twesleyb <tyler.w.bradshaw@duke.edu>
2020-04-07 13:18:14 -0400

    look at google search results

--- tutorials/google-search/google-search.py
+++ tutorials/google-search/google-search.py
@@ -19,10 +19,24 @@
 # Or, with Conda:
 # $ conda install -c conda-forge google-api-python-client
 
+# Imports.
+import subprocess
 from googleapiclient.discovery import build
 
-my_api_key = "AIzaSyBnkRwluy5yu0ESYUPSrIgwzAvo_gbhSw4"
-my_cse_id = "004723198555886821667:lr9hohyciei"
+# Define a function to get credentials from pass.
+def get_pass_credentials(entry):
+    ''' Get credentials from pass.
+    Extracts a dictionary of key:value pairs from entry
+    in password store. Expects delimiter to be ":".
+    '''
+    cmd = ["pass",entry]
+    process = subprocess.Popen(cmd,stdout=subprocess.PIPE)
+    output = process.communicate()
+    result = list(output)[0].decode('utf-8').split("\n")
+    result = [entry.split(": ") for entry in result]
+    result = [res for res in result if res != ['']]
+    credentials = {key: value for (key, value) in result}
+    return credentials
 
 # Define a function to search google.
 def google_search(search_term, api_key, cse_id, **kwargs):
@@ -31,8 +45,40 @@ def google_search(search_term, api_key, cse_id, **kwargs):
     result = query.execute()
     return result
 
-result = google_search("Coffee", my_api_key, my_cse_id)
+# Get credentials.
+credentials = get_pass_credentials("google-search-cse-id")
+
+# Search google.
+query = "Tyler Bradshaw"
+result = google_search(query, credentials.get('api_key'),credentials.get('cse_id'))
 
 # Parse the result.
-print(result)
+result.keys()
 
+# Information about the search query.
+result.get('queries')
+
+# What type of search was done.
+result.get('kind')
+
+# Api url?
+result.get('url')
+
+# API name.
+result.get('context')
+
+# Statistics about search.
+result.get('searchInformation')
+
+# Actual responses:
+# List of search results
+response = result.get('items')
+
+# Each entry in the list of responses is a dictionary.
+r0 = response[0]
+r0.keys()
+
+# Most useful bits:
+r0.get('title')
+r0.get('link')
+r0.get('snippet')
