1e604971b3bb57eb6cd0052aa26aa8bd93fc22fe
twesleyb10 <tyler.w.bradshaw@duke.edu>
2020-04-06 21:54:10 -0400

    todo

---
+++ tutorials/google-search/google-search.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+# From: https://linuxhint.com/google_search_api_python/
+
+# How to search google with python.
---
+++ tutorials/google-trends/scrape-trends.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+# Scape Google trends with python.
+# See library documentation: 
+# https://pypi.org/project/pytrends/
